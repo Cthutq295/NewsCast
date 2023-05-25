01cd39097a808d174bc9ce774f5b521e64c050c1
Sergey <fsk-2001@mail.ru>
2023-05-26 00:11:49 +0300

    Removed empty packages; Removed unused imports; Removed fps counter.

--- app/src/main/java/com/lazy/newscast/MainActivity.kt
+++ app/src/main/java/com/lazy/newscast/MainActivity.kt
@@ -2,9 +2,6 @@ package com.lazy.newscast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Choreographer
-import android.widget.ImageButton
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -12,7 +9,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.lazy.newscast.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.system.measureTimeMillis
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,57 +28,9 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
 
-        //startFPSCount()
-        ImageButton
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-
-    private fun startFPSCount() {
-
-        var frameCount = 0
-        var lastFrameTime = System.nanoTime()
-        var minFps = Float.MAX_VALUE
-        var totalFps = 0f
-
-        // Создаем список для хранения всех замеров FPS
-        val fpsList = mutableListOf<Float>()
-
-        val frameCallback = object : Choreographer.FrameCallback {
-            override fun doFrame(frameTimeNanos: Long) {
-                val currentTime = System.nanoTime()
-                val elapsedNanos = currentTime - lastFrameTime
-                val elapsedMs =
-                    elapsedNanos / 1_000_000f // Добавляем f, чтобы результат был типа Float
-                frameCount++
-
-                if (frameCount > 0 && elapsedMs > 1_000) { // Исправляем условие: если прошло ровно 1000 мс, то должно замеряться
-                    val fps = frameCount * 1_000f / elapsedMs
-                    totalFps += fps
-                    minFps = minOf(minFps, fps)
-
-                    fpsList.add(fps)
-                    val averageFps = if (fpsList.isNotEmpty()) {
-                        (fpsList.sum()) / (fpsList.size)
-                    } else {
-                        fps
-                    }
-
-                    // Выводим результаты в лог
-                    Log.i("FPS", "FPS: $fps, Min FPS: $minFps, Avg FPS: $averageFps")
-
-                    // Сбрасываем значения для следующего замера
-                    frameCount = 0
-                    lastFrameTime = currentTime
-                }
-
-                // Запускаем следующий замер FPS
-                Choreographer.getInstance().postFrameCallback(this)
-            }
-        }
-
-        Choreographer.getInstance().postFrameCallback(frameCallback)
     }
 }
