32fe353c345437c996d5deab60cdf1294acd7846
Sergey <fsk-2001@mail.ru>
2023-06-19 14:17:39 +0300

    Removed "Resource" class as it is not used anymore.

--- app/src/main/java/com/lazy/newscast/utils/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.lazy.newscast.utils
-
-sealed class Resource<T> {
-    data class Success<T>(val response: T) : Resource<T>()
-    class Loading<T> : Resource<T>()
-    class Error<T>(val message: String) : Resource<T>()
-}
